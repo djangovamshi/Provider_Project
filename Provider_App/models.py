6fee338f13124420078860f301674b9c086ba35a
djangovamshi <74848250+djangovamshi@users.noreply.github.com>
2021-10-06 15:09:51 +0530

    Update models.py

--- Provider_App/models.py
+++ Provider_App/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
-
+# This Model is Regarding The Employee Deatils....
 class Employee(models.Model):
     eno = models.IntegerField()
     ename  = models.CharField(max_length=50)
